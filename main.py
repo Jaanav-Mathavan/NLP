53bd03fe6a0ebb04f77cfa858ecd505a03906988
Shuban32 <bs21b032@smail.iitm.ac.in>
2025-05-12 05:33:50 +0530

    ESA/NESA with TF-IDF hybrid scoring

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tokenization import Tokenization
 from inflectionReduction import InflectionReduction
 from stopwordRemoval import StopwordRemoval
 from informationRetrieval import InformationRetrieval
+from esa import ExplicitSemanticAnalysis
 from evaluation import Evaluation
 import os
 from sys import version_info
@@ -20,248 +21,144 @@ elif version_info.major == 2:
     except NameError:
         pass
 else:
-    print ("Unknown python version - input function not safe")
-
+    print("Unknown python version - input function not safe")
 
 class SearchEngine:
+    def __init__(self, args):
+        self.args = args
+        self.tokenizer = Tokenization()
+        self.sentenceSegmenter = SentenceSegmentation()
+        self.inflectionReducer = InflectionReduction()
+        self.stopwordRemover = StopwordRemoval()
+        self.evaluator = Evaluation()
+        if args.model in ["esa", "nesa"]:
+            self.informationRetriever = ExplicitSemanticAnalysis(model_type=args.model)
+        else:
+            self.informationRetriever = InformationRetrieval()
 
-	def __init__(self, args):
-		self.args = args
+    def segmentSentences(self, text):
+        if self.args.segmenter == "naive":
+            return self.sentenceSegmenter.naive(text)
+        elif self.args.segmenter == "punkt":
+            return self.sentenceSegmenter.punkt(text)
 
-		self.tokenizer = Tokenization()
-		self.sentenceSegmenter = SentenceSegmentation()
-		self.inflectionReducer = InflectionReduction()
-		self.stopwordRemover = StopwordRemoval()
-		self.informationRetriever = InformationRetrieval()
-		self.evaluator = Evaluation()
+    def tokenize(self, text):
+        if self.args.tokenizer == "naive":
+            return self.tokenizer.naive(text)
+        elif self.args.tokenizer == "ptb":
+            return self.tokenizer.pennTreeBank(text)
 
+    def reduceInflection(self, text):
+        return self.inflectionReducer.reduce(text)
 
-	def segmentSentences(self, text):
-		"""
-		Call the required sentence segmenter
-		"""
-		if self.args.segmenter == "naive":
-			return self.sentenceSegmenter.naive(text)
-		elif self.args.segmenter == "punkt":
-			return self.sentenceSegmenter.punkt(text)
+    def removeStopwords(self, text):
+        return self.stopwordRemover.fromList(text)
 
-	def tokenize(self, text):
-		"""
-		Call the required tokenizer
-		"""
-		if self.args.tokenizer == "naive":
-			return self.tokenizer.naive(text)
-		elif self.args.tokenizer == "ptb":
-			return self.tokenizer.pennTreeBank(text)
+    def preprocessQueries(self, queries):
+        os.makedirs(self.args.out_folder, exist_ok=True)
+        segmentedQueries = [self.segmentSentences(query) for query in queries]
+        json.dump(segmentedQueries, open(self.args.out_folder + "segmented_queries.txt", 'w'))
+        tokenizedQueries = [self.tokenize(query) for query in segmentedQueries]
+        json.dump(tokenizedQueries, open(self.args.out_folder + "tokenized_queries.txt", 'w'))
+        reducedQueries = [self.reduceInflection(query) for query in tokenizedQueries]
+        json.dump(reducedQueries, open(self.args.out_folder + "reduced_queries.txt", 'w'))
+        stopwordRemovedQueries = [self.removeStopwords(query) for query in reducedQueries]
+        json.dump(stopwordRemovedQueries, open(self.args.out_folder + "stopword_removed_queries.txt", 'w'))
+        print("Sample preprocessed query tokens:", stopwordRemovedQueries[0][:10])
+        return stopwordRemovedQueries
 
-	def reduceInflection(self, text):
-		"""
-		Call the required stemmer/lemmatizer
-		"""
-		return self.inflectionReducer.reduce(text)
+    def preprocessDocs(self, docs):
+        segmentedDocs = [self.segmentSentences(doc) for doc in docs]
+        json.dump(segmentedDocs, open(self.args.out_folder + "segmented_docs.txt", 'w'))
+        tokenizedDocs = [self.tokenize(doc) for doc in segmentedDocs]
+        json.dump(tokenizedDocs, open(self.args.out_folder + "tokenized_docs.txt", 'w'))
+        reducedDocs = [self.reduceInflection(doc) for doc in tokenizedDocs]
+        json.dump(reducedDocs, open(self.args.out_folder + "reduced_docs.txt", 'w'))
+        stopwordRemovedDocs = [self.removeStopwords(doc) for doc in reducedDocs]
+        json.dump(stopwordRemovedDocs, open(self.args.out_folder + "stopword_removed_docs.txt", 'w'))
+        print("Sample preprocessed doc tokens:", stopwordRemovedDocs[0][:10])
+        return stopwordRemovedDocs
 
-	def removeStopwords(self, text):
-		"""
-		Call the required stopword remover
-		"""
-		return self.stopwordRemover.fromList(text)
+    def evaluateDataset(self):
+        queries_json = json.load(open(self.args.dataset + "cran_queries.json", 'r'))[:]
+        query_ids = [str(item["query number"]) for item in queries_json]  # Ensure string IDs
+        queries = [item["query"] for item in queries_json]
+        processedQueries = self.preprocessQueries(queries)
 
+        docs_json = json.load(open(self.args.dataset + "cran_docs.json", 'r'))[:]
+        doc_ids = [str(item["id"]) for item in docs_json]  # Ensure string IDs
+        docs = [item["body"] for item in docs_json]
+        processedDocs = self.preprocessDocs(docs)
 
-	def preprocessQueries(self, queries):
-		"""
-		Preprocess the queries - segment, tokenize, stem/lemmatize and remove stopwords
-		"""
-		os.makedirs(self.args.out_folder, exist_ok=True)
-		# Segment queries
-		segmentedQueries = []
-		for query in queries:
-			segmentedQuery = self.segmentSentences(query)
-			segmentedQueries.append(segmentedQuery)
-		json.dump(segmentedQueries, open(self.args.out_folder + "segmented_queries.txt", 'w'))
-		# Tokenize queries
-		tokenizedQueries = []
-		for query in segmentedQueries:
-			tokenizedQuery = self.tokenize(query)
-			tokenizedQueries.append(tokenizedQuery)
-		json.dump(tokenizedQueries, open(self.args.out_folder + "tokenized_queries.txt", 'w'))
-		# Stem/Lemmatize queries
-		reducedQueries = []
-		for query in tokenizedQueries:
-			reducedQuery = self.reduceInflection(query)
-			reducedQueries.append(reducedQuery)
-		json.dump(reducedQueries, open(self.args.out_folder + "reduced_queries.txt", 'w'))
-		# Remove stopwords from queries
-		stopwordRemovedQueries = []
-		for query in reducedQueries:
-			stopwordRemovedQuery = self.removeStopwords(query)
-			stopwordRemovedQueries.append(stopwordRemovedQuery)
-		json.dump(stopwordRemovedQueries, open(self.args.out_folder + "stopword_removed_queries.txt", 'w'))
+        self.informationRetriever.buildIndex(processedDocs, doc_ids)
+        doc_IDs_ordered = self.informationRetriever.rank(processedQueries)
 
-		preprocessedQueries = stopwordRemovedQueries
-		return preprocessedQueries
+        qrels = json.load(open(self.args.dataset + "cran_qrels.json", 'r'))[:]
 
-	def preprocessDocs(self, docs):
-		"""
-		Preprocess the documents
-		"""
-		
-		# Segment docs
-		segmentedDocs = []
-		for doc in docs:
-			segmentedDoc = self.segmentSentences(doc)
-			segmentedDocs.append(segmentedDoc)
-		json.dump(segmentedDocs, open(self.args.out_folder + "segmented_docs.txt", 'w'))
-		# Tokenize docs
-		tokenizedDocs = []
-		for doc in segmentedDocs:
-			tokenizedDoc = self.tokenize(doc)
-			tokenizedDocs.append(tokenizedDoc)
-		json.dump(tokenizedDocs, open(self.args.out_folder + "tokenized_docs.txt", 'w'))
-		# Stem/Lemmatize docs
-		reducedDocs = []
-		for doc in tokenizedDocs:
-			reducedDoc = self.reduceInflection(doc)
-			reducedDocs.append(reducedDoc)
-		json.dump(reducedDocs, open(self.args.out_folder + "reduced_docs.txt", 'w'))
-		# Remove stopwords from docs
-		stopwordRemovedDocs = []
-		for doc in reducedDocs:
-			stopwordRemovedDoc = self.removeStopwords(doc)
-			stopwordRemovedDocs.append(stopwordRemovedDoc)
-		json.dump(stopwordRemovedDocs, open(self.args.out_folder + "stopword_removed_docs.txt", 'w'))
+        # Debugging: Check ID consistency
+        print("Sample doc_id:", doc_ids[0], "Type:", type(doc_ids[0]))
+        print("Sample query_id:", query_ids[0], "Type:", type(query_ids[0]))
+        print("Sample qrels entry:", qrels[0])
+        doc_ids_set = set(doc_ids)
+        qrels_doc_ids = set(item['id'] for item in qrels)
+        print("Intersection of doc_ids and qrels:", len(doc_ids_set & qrels_doc_ids))
 
-		preprocessedDocs = stopwordRemovedDocs
-		return preprocessedDocs
+        precisions, recalls, fscores, MAPs, nDCGs = [], [], [], [], []
+        for k in range(1, 11):
+            precision = self.evaluator.meanPrecision(doc_IDs_ordered, query_ids, qrels, k)
+            precisions.append(precision)
+            recall = self.evaluator.meanRecall(doc_IDs_ordered, query_ids, qrels, k)
+            recalls.append(recall)
+            fscore = self.evaluator.meanFscore(doc_IDs_ordered, query_ids, qrels, k)
+            fscores.append(fscore)
+            print(f"Precision, Recall and F-score @ {k} : {precision}, {recall}, {fscore}")
+            MAP = self.evaluator.meanAveragePrecision(doc_IDs_ordered, query_ids, qrels, k)
+            MAPs.append(MAP)
+            nDCG = self.evaluator.meanNDCG(doc_IDs_ordered, query_ids, qrels, k)
+            nDCGs.append(nDCG)
+            print(f"MAP, nDCG @ {k} : {MAP}, {nDCG}")
 
+        plt.plot(range(1, 11), precisions, label="Precision")
+        plt.plot(range(1, 11), recalls, label="Recall")
+        plt.plot(range(1, 11), fscores, label="F-Score")
+        plt.plot(range(1, 11), MAPs, label="MAP")
+        plt.plot(range(1, 11), nDCGs, label="nDCG")
+        plt.legend()
+        plt.title("Evaluation Metrics - Cranfield Dataset")
+        plt.xlabel("k")
+        plt.savefig(self.args.out_folder + "eval_plot.png")
 
-	def evaluateDataset(self):
-		"""
-		- preprocesses the queries and documents, stores in output folder
-		- invokes the IR system
-		- evaluates precision, recall, fscore, nDCG and MAP 
-		  for all queries in the Cranfield dataset
-		- produces graphs of the evaluation metrics in the output folder
-		"""
-
-		# Read queries
-		queries_json = json.load(open(args.dataset + "cran_queries.json", 'r'))[:]
-		query_ids, queries = [item["query number"] for item in queries_json], \
-								[item["query"] for item in queries_json]
-		# Process queries 
-		processedQueries = self.preprocessQueries(queries)
-
-		# Read documents
-		docs_json = json.load(open(args.dataset + "cran_docs.json", 'r'))[:]
-		doc_ids, docs = [item["id"] for item in docs_json], \
-								[item["body"] for item in docs_json]
-		# Process documents
-		processedDocs = self.preprocessDocs(docs)
-
-		# Build document index
-		self.informationRetriever.buildIndex(processedDocs, doc_ids)
-		# Rank the documents for each query
-		doc_IDs_ordered = self.informationRetriever.rank(processedQueries)
-
-		# Read relevance judements
-		qrels = json.load(open(args.dataset + "cran_qrels.json", 'r'))[:]
-
-		# Calculate precision, recall, f-score, MAP and nDCG for k = 1 to 10
-		precisions, recalls, fscores, MAPs, nDCGs = [], [], [], [], []
-		for k in range(1, 11):
-			precision = self.evaluator.meanPrecision(
-				doc_IDs_ordered, query_ids, qrels, k)
-			precisions.append(precision)
-			recall = self.evaluator.meanRecall(
-				doc_IDs_ordered, query_ids, qrels, k)
-			recalls.append(recall)
-			fscore = self.evaluator.meanFscore(
-				doc_IDs_ordered, query_ids, qrels, k)
-			fscores.append(fscore)
-			print("Precision, Recall and F-score @ " +  
-				str(k) + " : " + str(precision) + ", " + str(recall) + 
-				", " + str(fscore))
-			MAP = self.evaluator.meanAveragePrecision(
-				doc_IDs_ordered, query_ids, qrels, k)
-			MAPs.append(MAP)
-			nDCG = self.evaluator.meanNDCG(
-				doc_IDs_ordered, query_ids, qrels, k)
-			nDCGs.append(nDCG)
-			print("MAP, nDCG @ " +  
-				str(k) + " : " + str(MAP) + ", " + str(nDCG))
-
-		# Plot the metrics and save plot 
-		plt.plot(range(1, 11), precisions, label="Precision")
-		plt.plot(range(1, 11), recalls, label="Recall")
-		plt.plot(range(1, 11), fscores, label="F-Score")
-		plt.plot(range(1, 11), MAPs, label="MAP")
-		plt.plot(range(1, 11), nDCGs, label="nDCG")
-		plt.legend()
-		plt.title("Evaluation Metrics - Cranfield Dataset")
-		plt.xlabel("k")
-		plt.savefig(args.out_folder + "eval_plot.png")
-
-		
-	def handleCustomQuery(self):
-		"""
-		Take a custom query as input and return top five relevant documents
-		"""
-
-		#Get query
-		print("Enter query below")
-		query = input()
-		custom_start_time = time.time()
-		# Process documents
-		processedQuery = self.preprocessQueries([query])[0]
-
-		# Read documents
-		docs_json = json.load(open(args.dataset + "cran_docs.json", 'r'))[:]
-		doc_ids, docs = [item["id"] for item in docs_json], \
-							[item["body"] for item in docs_json]
-		# Process documents
-		processedDocs = self.preprocessDocs(docs)
-
-		# Build document index
-		self.informationRetriever.buildIndex(processedDocs, doc_ids)
-		# Rank the documents for the query
-		doc_IDs_ordered = self.informationRetriever.rank([processedQuery])[0]
-		custom_end_time = time.time()
-		print("Custom Query Time taken : " + str(custom_end_time - custom_start_time) + " seconds")
-		# Print the IDs of first five documents
-		print("\nTop five document IDs : ")
-		for id_ in doc_IDs_ordered[:5]:
-			print(id_)
-
-
+    def handleCustomQuery(self):
+        print("Enter query below")
+        query = input()
+        custom_start_time = time.time()
+        processedQuery = self.preprocessQueries([query])[0]
+        docs_json = json.load(open(self.args.dataset + "cran_docs.json", 'r'))[:]
+        doc_ids = [str(item["id"]) for item in docs_json]  # Ensure string IDs
+        docs = [item["body"] for item in docs_json]
+        processedDocs = self.preprocessDocs(docs)
+        self.informationRetriever.buildIndex(processedDocs, doc_ids)
+        doc_IDs_ordered = self.informationRetriever.rank([processedQuery])[0]
+        custom_end_time = time.time()
+        print("Custom Query Time taken : " + str(custom_end_time - custom_start_time) + " seconds")
+        print("\nTop five document IDs : ")
+        for id_ in doc_IDs_ordered[:5]:
+            print(id_)
 
 if __name__ == "__main__":
-
-	# Create an argument parser
-	parser = argparse.ArgumentParser(description='main.py')
-
-	# Tunable parameters as external arguments
-	parser.add_argument('-dataset', default = "cranfield/", 
-						help = "Path to the dataset folder")
-	parser.add_argument('-out_folder', default = "output/", 
-						help = "Path to output folder")
-	parser.add_argument('-segmenter', default = "punkt",
-	                    help = "Sentence Segmenter Type [naive|punkt]")
-	parser.add_argument('-tokenizer',  default = "ptb",
-	                    help = "Tokenizer Type [naive|ptb]")
-	parser.add_argument('-custom', action = "store_true", 
-						help = "Take custom query as input")
-	
-	# Parse the input arguments
-	args = parser.parse_args()
-
-	# Create an instance of the Search Engine
-	searchEngine = SearchEngine(args)
-
-	# Either handle query from user or evaluate on the complete dataset 
-	start_time = time.time()
-	if args.custom:
-		searchEngine.handleCustomQuery()
-	else:
-		searchEngine.evaluateDataset()
-		end_time = time.time()
-		print("Cranfield Query Dataset Time taken : " + str(end_time - start_time) + " seconds")
-	
+    parser = argparse.ArgumentParser(description='main.py')
+    parser.add_argument('-dataset', default="cranfield/", help="Path to the dataset folder")
+    parser.add_argument('-out_folder', default="output/", help="Path to output folder")
+    parser.add_argument('-segmenter', default="punkt", help="Sentence Segmenter Type [naive|punkt]")
+    parser.add_argument('-tokenizer', default="ptb", help="Tokenizer Type [naive|ptb]")
+    parser.add_argument('-custom', action="store_true", help="Take custom query as input")
+    parser.add_argument('-model', default="esa", help="IR model [esa|nesa|tfidf]")
+    args = parser.parse_args()
+    searchEngine = SearchEngine(args)
+    start_time = time.time()
+    if args.custom:
+        searchEngine.handleCustomQuery()
+    else:
+        searchEngine.evaluateDataset()
+        end_time = time.time()
+        print("Cranfield Query Dataset Time taken : " + str(end_time - start_time) + " seconds")
--- tokenization.py
+++ tokenization.py
@@ -1,81 +1,47 @@
 from util import *
-import nltk 
+import re
 from nltk.tokenize.treebank import TreebankWordTokenizer
-# Add your import statements here
 
+class Tokenization:
+    def naive(self, text):
+        """
+        Tokenization using a Naive Approach, removing punctuation
 
+        Parameters
+        ----------
+        text : list
+            A list of strings where each string is a single sentence
 
+        Returns
+        -------
+        list
+            A list of lists where each sub-list is a sequence of alphanumeric tokens
+        """
+        tokenizedText = []
+        for sentence in text:
+            # Match only alphanumeric words, exclude punctuation
+            tokens = [token.lower() for token in re.findall(r'\b[a-zA-Z0-9]+\b', sentence.lower())]
+            tokenizedText.append(tokens)
+        return tokenizedText
 
-class Tokenization():
-	def __init__(self):
-		#If I import nltk.tokenize.word_tokenize then it includes TreebankWordTokenizer along with PunktSentenceTokenizer. So I am specifically using TreebankWordTokenizer
-		self.word_tokenizer = TreebankWordTokenizer()
-  		#Exceptions for Abbreviations
-		self.exceptions = ["Mr", "Mrs", "Ms", "Dr", "Prof", "Inc", "no", "rev", "Ltd" "e.g", "i.e", "a.m", "p.m"]
-		#Numbers such as 1., 2., 3. should also be considered as abbreviations in order to prevent being seperated
-		self.exceptions += [str(i) for i in range(10)] 
-  		#Unlike sentences here even brackets need to be considered a separate token
-		self.punctuations = ".,!?;:'\"()[]{}" 
-	def naive(self, text):
-		"""
-		Tokenization using a Naive Approach
+    def pennTreeBank(self, text):
+        """
+        Tokenization using the Penn Tree Bank Tokenizer, removing punctuation
 
-		Parameters
-		----------
-		arg1 : list
-			A list of strings where each string is a single sentence
+        Parameters
+        ----------
+        text : list
+            A list of strings where each string is a single sentence
 
-		Returns
-		-------
-		list
-			A list of lists where each sub-list is a sequence of tokens
-		"""
-
-		tokenizedText = None
-		tokenizedText = []
-		for segment in text:
-			tokens = []
-			current_word = ""
-			for char in segment:
-       			#Check for alpha numebric characters or apostrophe
-				if char.isalnum() or char == "'":
-					current_word += char 
-				else:
-					#Decimal places, exceptions based words
-					if current_word and current_word in self.exceptions and char == ".":
-						current_word += char
-					if current_word:
-						tokens.append(current_word)
-						current_word = "" 
-					#Append to tokens seperately if char is one of the punctuations. We arent removing any punctuations for now.
-					if char in self.punctuations or char in "+-/*^&@#%^":
-						tokens.append(char)
-			tokenizedText.append(tokens)
-					
-		#Fill in code here
-
-		return tokenizedText
-
-
-
-	def pennTreeBank(self, text):
-		"""
-		Tokenization using the Penn Tree Bank Tokenizer
-
-		Parameters
-		----------
-		arg1 : list
-			A list of strings where each string is a single sentence
-
-		Returns
-		-------
-		list
-			A list of lists where each sub-list is a sequence of tokens
-		"""
-		tokenizedText = None
-		tokenizedText = []
-		for segment in text:
-			tokenizedText.append(self.word_tokenizer.tokenize(segment))
-		#Fill in code here
-
-		return tokenizedText
+        Returns
+        -------
+        list
+            A list of lists where each sub-list is a sequence of alphanumeric tokens
+        """
+        tokenizedText = []
+        tokenizer = TreebankWordTokenizer()
+        for sentence in text:
+            # Filter out non-alphanumeric tokens
+            tokens = [token.lower() for token in tokenizer.tokenize(sentence) if token.isalnum()]
+            tokenizedText.append(tokens)
+        return tokenizedText
